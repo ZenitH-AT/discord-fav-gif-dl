81349bc8aef4f911fef308bfb696fba3483e251c
ZenitH-AT <zenith3za@gmail.com>
2023-02-14 08:06:17 +0200

    rename run.py to get_gifs.py

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # discord-fav-gif-dl
-Python script to download all your favourited GIFs on Discord.
+
+Downloads all your favourited GIFs on Discord.
+
+## Usage
+
+- Save your GIFFavoritesStore to a file
+    - Open Developer Tools in your browser or Discord's desktop application
+    - Navigate to the `Application` tab > `Local Storage` > `https://discord.com` > `GIFFavoritesStore`
+        - If using the Discord desktop application, you may need to first enable Developer Tools by adding the following line to your `%APPDATA%/discord/settings.json` file:
+
+            ```json
+            "DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING": true
+            ```
+    - Copy the contents of the `Value` field and save it to a file (e.g. `data.json`)
+- Install required packages by running `python -m pip install -r requirements.txt`
+- Run the script using `python get_gifs.py --json-file-path <path to GIFFavoritesStore data file>`
+    - You can use the `--dont-rename-files` option to download files with their original names, instead of renaming them to hex strings.
---
+++ get_gifs.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import json
+import requests
+import argparse
+
+def download_gifs(json_file_path: str, dont_rename_files: bool):
+    with open(json_file_path, "r") as json_file:
+        data = json.load(json_file)
+
+    gifs = data["_state"]["favorites"]
+    failed_urls = []
+
+    for i, gif in enumerate(reversed(gifs)):
+        url = gif["src"].strip("/")
+
+        if not url.startswith(("http://", "https://")):
+            url = f"http://{url}" 
+
+        file_name = url.rsplit("/", 1)[-1] if dont_rename_files else f"{hex(i)[2:]}.{url.rsplit('/', 1)[-1].rsplit('.', 1)[-1]}"
+        path = Path(f"output/{file_name}")
+        progress = f"[{i + 1}/{len(gifs)}] {path} "
+
+        print(progress, end="")
+
+        if path.exists():
+            print("[already downloaded]")
+
+            continue
+
+        response = requests.get(url)
+        content = response.content
+
+        if not (response.ok and content):
+            failed_urls.append(f"{progress}- {url}")
+            print("❌")
+
+            continue
+
+        with open(path, "wb") as gif_file:
+            gif_file.write(content)
+
+        print("✔️")
+
+    if failed_urls:
+        with open("failed.txt", "w") as text_file:
+            text_file.write("\n".join(failed_urls))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--json-file-path", "-j", type=str, required=True, help="Path to GIFFavoritesStore JSON file")
+    parser.add_argument("--dont-rename-files", "-d", type=bool, default=True, help="Whether to keep original file names")
+
+    args = parser.parse_args()
+
+    download_gifs(json_file_path=args.json_file_path, rename_files=args.dont_rename_files)
